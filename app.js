2aee2d5fbb35924062d54af84f91abb42ecbdfa3
Nicolo Danna <ndanna128@gmail.com>
2022-02-25 16:28:52 -0500

    added inquirer question prompt to capture user input

--- app.js
+++ app.js
@@ -14,33 +14,50 @@
 
 // printProfileData(profileDataArgs);
 
-// the require statment is a built in function thats globally available in Node.js. Allows app.js to access the fs modules functions through the fs assignment
-const fs = require('fs');
-//This statement the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js
-const generatePage = require('./src/page-template');
 
-// Array that holds the user command line argument.
-const profileDataArgs = process.argv.slice(2);
+// // Array that holds the user command line argument.
+// const profileDataArgs = process.argv.slice(2);
 
-console.log(profileDataArgs);
+// console.log(profileDataArgs);
 
-//Extracted arguments stored into variables. This is called an Array Index expression.
-const [name, github] = profileDataArgs;
+// //Extracted arguments stored into variables. This is called an Array Index expression.
+// const [name, github] = profileDataArgs;
 
-console.log(name, github);
-
-//variable for generatePage function
-const pageHTML = generatePage(name, github);
+// console.log(name, github);
 
 
 
+//Inquirer can recieve a question object with properties like "type, name, message".
+const inquirer = require('inquirer');
 
-//File system function. 'index.html' is the file name that will be created
-// generatePage(name,github) is the data thats being written: the HTML template
-// err => is the call back function that will handle any errors as well as a success message.
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// // the require statment is a built in function thats globally available in Node.js. Allows app.js to access the fs modules functions through the fs assignment
+// const fs = require('fs');
+// //This statement the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js
+// const generatePage = require('./src/page-template');
 
-    //success statment that directs users to inspect the newly created file
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+
+// //variable for generatePage function
+// const pageHTML = generatePage(name, github);
+
+
+
+
+// //File system function. 'index.html' is the file name that will be created
+// // generatePage(name,github) is the data thats being written: the HTML template
+// // err => is the call back function that will handle any errors as well as a success message.
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+
+//     //success statment that directs users to inspect the newly created file
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
